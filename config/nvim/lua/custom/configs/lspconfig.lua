f621763b354f768ff38f67cd6f2ddc809bb8db3b
n6v26r <dedu.razvan.matei@gmail.com>
2023-10-08 13:15:06 +0300

    Improved nvim csharp lsp speed

--- config/nvim/lua/custom/configs/lspconfig.lua
+++ config/nvim/lua/custom/configs/lspconfig.lua
@@ -10,14 +10,17 @@ lspconfig.marksman.setup({
   single_file_support = true;
 })
 
-lspconfig.omnisharp.setup({
-	cmd = { "dotnet", "/usr/lib/omnisharp-roslyn/OmniSharp.dll"},
-  single_file_support = true,
-  analyze_open_documents_only = true,
-})
+-- Faster csharp_ls load time with omnisharp disabled. Enable it, and remove csharp_ls from server list 
+-- if you would like to use omni (which also has singe file support). In my exp, omnisharp is slower.
+-- lspconfig.omnisharp.setup({
+-- 	cmd = { "dotnet", "/usr/lib/omnisharp-roslyn/OmniSharp.dll"},
+--   single_file_support = true,
+--   analyze_open_documents_only = true,
+-- })
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "csharp_ls" }
+
+local servers = { "html", "cssls", "tsserver", "clangd", "csharp_ls"}
 
 capabilities.offsetEncoding = {"utf-16"}
 
